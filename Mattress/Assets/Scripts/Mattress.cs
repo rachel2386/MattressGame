00bf7c3d2eb3a3f1c1e80ee37d337a445829211c
Siyuan.Q <siyuan.qiu@squinopera.com>
2019-01-25 23:31:17 -0500

    Added scripts for UDP gyro info litsening

---
+++ Mattress/Assets/Scripts/Mattress.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Mattress : MonoBehaviour
+{
+    [SerializeField] Vector2 _pinchRange = new Vector2(-3.13f, 3.13f);
+    [SerializeField] Vector2 _rollRange = new Vector2(-3.13f, 3.13f);
+    [SerializeField] Vector2 _yawRange = new Vector2(-3.13f, 3.13f);
+
+    private void OnEnable()
+    {
+        UDPListenser.UDPReceived += OnGyroDataReceived;
+    }
+
+    private void OnDisable()
+    {
+        UDPListenser.UDPReceived -= OnGyroDataReceived;
+    }
+
+    private void OnGyroDataReceived(string dataString, float interval)
+    {
+        string[] latestData = dataString.Split(',');
+        float z = Mathf.Lerp(180f, -180f, (float.Parse(latestData[1]) - _rollRange.x) / (_rollRange.y - _rollRange.x));
+        float y = Mathf.Lerp(180f, -180f, (float.Parse(latestData[3]) - _yawRange.x) / (_yawRange.y - _yawRange.x));
+        float x = Mathf.Lerp(180f, -180f, (float.Parse(latestData[2]) - _pinchRange.x) / (_pinchRange.y - _pinchRange.x));
+
+        if (interval > 0.1f)
+        {
+            transform.DORotate(new Vector3(x, y, z), interval, RotateMode.Fast).SetEase(Ease.Linear);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(x, y, z);
+        }
+    }
+}
---
+++ Mattress/Assets/Scripts/UDPListenser.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class UDPListenser : MonoBehaviour
+{
+
+    public static event Action<string, float> UDPReceived;
+
+    [SerializeField] private int _port = 10552;
+
+    private UdpClient _client;
+    private IPEndPoint _remoteIP;
+    private Thread _udpThread;
+
+    private float _lastUpdateTime = 0;
+
+    private List<string> _receivedData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Setup(_port);
+    }
+
+    public void Setup(int port)
+    {
+        _receivedData = new List<string>();
+
+        _client = new UdpClient(port);
+        _remoteIP = new IPEndPoint(IPAddress.Any, 0);
+        _udpThread = new Thread(new ThreadStart(UDPRead));
+        _udpThread.IsBackground = true;
+        _udpThread.Start();
+
+        Debug.Log("UDPListener Initialized:" + " Listening to: " + _remoteIP + " Port: " + port);
+    }
+
+    private void OnDestroy()
+    {
+#if UNITY_EDITOR
+        // This is only called in builds, so simulate it inside editor as OnDestroy will always be called
+        OnApplicationQuit();
+#endif
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (_udpThread != null) _udpThread.Abort();
+        if (_client != null) _client.Close();
+    }
+
+    private void UDPRead()
+    {
+        while (true)
+        {
+            try
+            {
+                byte[] receiveBytes = _client.Receive(ref _remoteIP);
+                if (receiveBytes != null && receiveBytes.Length != 0)
+                {
+                    string returnData = Encoding.ASCII.GetString(receiveBytes);
+                    if (returnData.Length != 0)
+                    {
+                        _receivedData.Add(returnData);
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Log("[BasicUDPListener.UDPRead] Exception: " + e.Message);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (_receivedData.Count > 0)
+        {
+            UDPReceived?.Invoke(_receivedData[_receivedData.Count - 1], Time.time - _lastUpdateTime);
+            _lastUpdateTime = Time.time;
+            _receivedData.Clear();
+        }
+    }
+}
