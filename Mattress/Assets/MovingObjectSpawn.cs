7993419c4b391b51fd6f4522637958b4d5c3ad22
Siyuan.Q <siyuan.qiu@squinopera.com>
2019-01-27 12:42:16 -0500

    Added moving object spawner and changed the rotation of the subway entrance

---
+++ Mattress/Assets/MovingObjectSpawn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MovingObjectSpawn : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _objectList;
+    [SerializeField] private float _width = 0;
+    [SerializeField] private float _movingDistance = 50f;
+    [SerializeField] private float _movingSpeed = 2f;
+    [SerializeField] private float _spawnMaxInterval = 7.0f;
+    [SerializeField] private float _spawnIntervalRandomness = 3.0f;
+
+    private float _timer = 0;
+    private float _randomIntervalOffset;
+
+    private void Start()
+    {
+        _randomIntervalOffset = Random.Range(0f, _spawnIntervalRandomness);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_timer > _spawnMaxInterval - _randomIntervalOffset)
+        {
+            _timer = 0;
+            _randomIntervalOffset = Random.Range(0f, _spawnIntervalRandomness);
+            GameObject movingObject = Instantiate(_objectList[Random.Range(0, _objectList.Length)], transform.position, transform.rotation, transform);
+            movingObject.transform.DOMove(transform.position + transform.forward * _movingDistance, _movingDistance / _movingSpeed)
+                .OnComplete(() => { Destroy(movingObject); });
+        }
+    }
+}
---
+++ Mattress/Assets/ObjectActiveSwitch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectActiveSwitch : MonoBehaviour
+{
+    [SerializeField] float _switchInterval = 20.0f;
+
+    [SerializeField] GameObject[] _groupA;
+    [SerializeField] GameObject[] _groupB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void EnableObjectGroup (GameObject[] group)
+    {
+        foreach(GameObject go in group)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
