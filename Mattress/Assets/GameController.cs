764b94bed4317b948afb556863c8d3dce23ecdae
Siyuan.Q <siyuan.qiu@squinopera.com>
2019-01-27 16:21:21 -0500

    Added end scene

--- Mattress/Assets/GameController.cs
+++ Mattress/Assets/GameController.cs
@@ -9,6 +9,7 @@ public class GameController : MonoBehaviour
     public static GameController Instance;
 
     [SerializeField] private LayerMask _endGameLayerMask;
+    [SerializeField] private CanvasGroup _endCanvas;
 
     private void Awake()
     {
@@ -24,7 +25,8 @@ public class GameController : MonoBehaviour
         Quaternion cameraRotation = Camera.main.transform.rotation;
         Camera.main.transform.parent = null;
         Destroy(PlayerMovement.Player.gameObject);
-        MattressGyroRotation.Instance.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 2 + Vector3.up * 0.1f;
+        _endCanvas.gameObject.SetActive(true);
+        MattressGyroRotation.Instance.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 3 + Vector3.up * 0.3f;
         MattressGyroRotation.Instance.transform.DORotate(Vector3.up * 360f, 10.0f, RotateMode.LocalAxisAdd).SetLoops(-1)
             .OnStart(()=> {
                 Camera.main.transform.position = cameraPosition;
