95cf9156be0b14f50f3ca68ede021d196b799ffc
Siyuan.Q <siyuan.qiu@squinopera.com>
2019-01-26 23:44:33 -0500

    Changed player movement to the fixed speed

--- Mattress/Assets/Scripts/PlayerMovement.cs
+++ Mattress/Assets/Scripts/PlayerMovement.cs
@@ -10,13 +10,17 @@ public class PlayerMovement : MonoBehaviour
     public Collider BaseCollider { get; private set; }
 
     [Header("Movement Settings")]
+    [SerializeField] private float _stepLength = 0.05f;
+    /*
     [SerializeField] private float _forwardForceFactor = 100000f;
     [SerializeField] private float _rightForceFactor = 100000f;
     [SerializeField] private float _movementGyroThreshold = 0.08f;
+    */
     [Header("Rotation Settings")]
     [SerializeField] Vector2 _yawRange = new Vector2(-3.13f, 3.13f);
 
     private Rigidbody _rigibody;
+    private object _movementTweenTarget = new object();
 
     private void Awake()
     {
@@ -42,6 +46,34 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnGyroDataReceived(Vector3 gyroVector, float interval)
     {
+        Vector3 walkDirection = Vector3.zero;
+        if (Mathf.Abs(gyroVector.x) > Mathf.Abs(gyroVector.z))
+        {
+            if (gyroVector.x > 0)
+            {
+                walkDirection = -transform.forward;
+            }
+            else
+            {
+                walkDirection = transform.forward;
+            }
+        }
+        else
+        {
+            if (gyroVector.z > 0)
+            {
+                walkDirection = transform.right;
+            }
+            else
+            {
+                walkDirection = -transform.right;
+            }
+        }
+
+        walkDirection.y = 0;
+
+
+        /*
         if (Mathf.Abs(gyroVector.x) > _movementGyroThreshold)
         {
             float forwardBeyongThreshold = (gyroVector.x / Mathf.Abs(gyroVector.x)) * (Mathf.Abs(gyroVector.x) - _movementGyroThreshold);
@@ -53,9 +85,16 @@ public class PlayerMovement : MonoBehaviour
             float rightBeyongThreshold = (gyroVector.z / Mathf.Abs(gyroVector.z)) * (Mathf.Abs(gyroVector.z) - _movementGyroThreshold);
             _rigibody.AddForce(rightBeyongThreshold * transform.right * _rightForceFactor, ForceMode.Force);
         }
+        */
 
         float y = Mathf.Lerp(180f, -180f, (gyroVector.y - _yawRange.x) / (_yawRange.y - _yawRange.x));
-        _rigibody.DOKill();
-        _rigibody.DORotate(Vector3.up * y, interval, RotateMode.Fast).SetEase(Ease.Linear);
+        DOTween.Kill(_movementTweenTarget);
+        _rigibody.DOMove(_rigibody.position + walkDirection.normalized * _stepLength, interval).SetEase(Ease.Linear).SetTarget(_movementTweenTarget);
+        _rigibody.DORotate(Vector3.up * y, interval, RotateMode.Fast).SetEase(Ease.Linear).SetTarget(_movementTweenTarget);
+
+        if (!DOTween.IsTweening(Camera.main.transform, true))
+        {
+            Camera.main.transform.DOLocalJump(Camera.main.transform.localPosition, 0.01f, 1, interval * 6f).SetEase(Ease.Linear);
+        }
     }
 }
