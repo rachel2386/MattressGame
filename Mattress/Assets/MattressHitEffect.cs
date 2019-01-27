34dd2222b4cc9bb92c0a689a78ea880be475c1f7
Siyuan.Q <siyuan.qiu@squinopera.com>
2019-01-27 16:41:24 -0500

    Added hit effect

--- Mattress/Assets/MattressHitEffect.cs
+++ Mattress/Assets/MattressHitEffect.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 [RequireComponent(typeof(AudioSource))]
 public class MattressHitEffect : MonoBehaviour
 {
     [SerializeField] private AudioClip[] _hitSounds;
+    [SerializeField] private Sprite[] _overlayDirtList;
+    [SerializeField] private CanvasGroup _dirtOverlayCanvas;
+    [SerializeField] private Image _leftDirtImage;
+    [SerializeField] private Image _rightDirtImage;
 
     AudioSource _audioSource;
 
@@ -23,5 +29,22 @@ public class MattressHitEffect : MonoBehaviour
     public void OnMattressHit (RaycastHit raycastHit)
     {
         _audioSource.PlayOneShot(_hitSounds[UnityEngine.Random.Range(0, _hitSounds.Length)]);
+        Vector3 rayDir = raycastHit.point - PlayerMovement.Player.transform.position;
+        if (Vector3.Dot(rayDir, PlayerMovement.Player.transform.right) > 0)
+        {
+            _leftDirtImage.gameObject.SetActive(false);
+            _rightDirtImage.gameObject.SetActive(true);
+        }
+        else
+        {
+            _leftDirtImage.gameObject.SetActive(true);
+            _rightDirtImage.gameObject.SetActive(false);
+        }
+        _leftDirtImage.sprite = _overlayDirtList[Random.Range(0, _overlayDirtList.Length)];
+        _leftDirtImage.sprite = _overlayDirtList[Random.Range(0, _overlayDirtList.Length)];
+
+        _dirtOverlayCanvas.alpha = 1;
+        _dirtOverlayCanvas.DOKill();
+        _dirtOverlayCanvas.DOFade(0, 0.5f);
     }
 }
